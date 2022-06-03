d48fae0d0874140a97a844175354ae18cd0e42d7
c_Door in <egor1@mail.ru>
2022-06-03 14:36:41 +0300

    Make logging to admin tg bot

--- tg_bot.py
+++ tg_bot.py
@@ -1,13 +1,27 @@
 import logging
 from textwrap import dedent
+from time import sleep
 
+import telegram
 from environs import Env
 from google.cloud import dialogflow
-from telegram import ForceReply, Update
+from telegram import ForceReply
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 
 
 logger = logging.getLogger(__name__)
+
+
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 async def start(update, context):
@@ -45,10 +59,21 @@ def main():
     env = Env()
     env.read_env()
 
-    application = ApplicationBuilder().token(env.str("TG_TOKEN")).build()
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-    application.run_polling()
+    tg_admin_chat_id = env.str('TG_ADMIN_CHAT_ID')
+    tg_admin_bot = telegram.Bot(token=env.str('TG_ADMIN_BOT_TOKEN'))
+    tg_logs_handler = TelegramLogsHandler(tg_admin_bot, tg_admin_chat_id)
+    tg_logs_handler.setLevel(logging.WARNING)
+    logger.addHandler(tg_logs_handler)
+
+    while True:
+        try:
+            application = ApplicationBuilder().token(env.str("TG_TOKEN")).build()
+            application.add_handler(CommandHandler("start", start))
+            application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+            application.run_polling()
+        except Exception:
+            logger.exception('Ошибка в game-of-verbs-help-tg-bot. Перезапуск через 15 секунд.')
+            sleep(15)
 
 
 if __name__ == "__main__":
--- vk_bot.py
+++ vk_bot.py
@@ -1,7 +1,9 @@
 import logging
 import random
 from textwrap import dedent
+from time import sleep
 
+import telegram
 import vk_api as vk
 from environs import Env
 from google.cloud import dialogflow
@@ -9,6 +11,18 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 
 
 logger = logging.getLogger(__name__)
+
+
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def detect_intent_texts(project_id, session_id, text, language_code):
@@ -44,26 +58,36 @@ def main():
     env = Env()
     env.read_env()
 
-    dialogflow_project_id = env.str('DIALOGFLOW_PROJECT_ID')
+    tg_admin_chat_id = env.str('TG_ADMIN_CHAT_ID')
+    tg_admin_bot = telegram.Bot(token=env.str('TG_ADMIN_BOT_TOKEN'))
+    tg_logs_handler = TelegramLogsHandler(tg_admin_bot, tg_admin_chat_id)
+    tg_logs_handler.setLevel(logging.WARNING)
+    logger.addHandler(tg_logs_handler)
 
-    vk_session = vk.VkApi(token=env.str('VK_TOKEN'))
-    vk_api = vk_session.get_api()
-    longpoll = VkLongPoll(vk_session)
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            response_text = detect_intent_texts(
-                dialogflow_project_id,
-                event.user_id,
-                event.text,
-                'Russian-ru',
-            )
-            logger.debug(dedent(f'''
-                Новое сообщение:
-                Для меня от: {event.user_id}
-                Текст: {event.text}'''
-            ))
-            if response_text:
-                echo(event, vk_api, response_text)
+    while True:
+        try:
+            dialogflow_project_id = env.str('DIALOGFLOW_PROJECT_ID')
+            vk_session = vk.VkApi(token=env.str('VK_TOKEN'))
+            vk_api = vk_session.get_api()
+            longpoll = VkLongPoll(vk_session)
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                    response_text = detect_intent_texts(
+                        dialogflow_project_id,
+                        event.user_id,
+                        event.text,
+                        'Russian-ru',
+                    )
+                    logger.debug(dedent(f'''
+                        Новое сообщение:
+                        Для меня от: {event.user_id}
+                        Текст: {event.text}'''
+                    ))
+                    if response_text:
+                        echo(event, vk_api, response_text)
+        except Exception:
+            logger.exception('Ошибка в game-of-verbs-help-vk-bot. Перезапуск через 15 секунд.')
+            sleep(15)
 
 
 if __name__ == "__main__":
