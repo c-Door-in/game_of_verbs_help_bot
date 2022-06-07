c5b87c51fe7fc0304c800cf69c9265d240a381b6
c_Door in <egor1@mail.ru>
2022-06-07 16:34:43 +0300

    Get intent file from local path

--- create_intent.py
+++ create_intent.py
@@ -1,26 +1,30 @@
 import argparse
-import requests
+import json
+import pathlib
 from environs import Env
 from google.cloud import dialogflow
 
 
-def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+def create_intent(project_id, intent_name, intent_raw):
 
     intents_client = dialogflow.IntentsClient()
 
     parent = dialogflow.AgentsClient.agent_path(project_id)
     training_phrases = []
-    for training_phrases_part in training_phrases_parts:
+    for training_phrases_part in intent_raw['questions']:
         part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
         # Here we create a new training phrase for each provided part.
         training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
         training_phrases.append(training_phrase)
 
+    message_texts = intent_raw['answer']
+    if type(message_texts) is not list:
+        message_texts = [message_texts]
     text = dialogflow.Intent.Message.Text(text=message_texts)
     message = dialogflow.Intent.Message(text=text)
 
     intent = dialogflow.Intent(
-        display_name=display_name, training_phrases=training_phrases, messages=[message]
+        display_name=intent_name, training_phrases=training_phrases, messages=[message]
     )
 
     response = intents_client.create_intent(
@@ -33,21 +37,17 @@ def create_intent(project_id, display_name, training_phrases_parts, message_text
 def main():
     env = Env()
     env.read_env()
+    dialogflow_project_id = env.str('DIALOGFLOW_PROJECT_ID')
 
     parser = argparse.ArgumentParser(description='Create intent with training phrases')
-    parser.add_argument('training_phrases_url', type=str, help='URL path to training phrases file.')
-    parser.add_argument('display_name', type=str, help='Intent name')
+    parser.add_argument('intents_json', type=pathlib.Path, help='Path to training phrases file.')
     args = parser.parse_args()
-    
-    response = requests.get(args.training_phrases_url)
-    response.raise_for_status()
-    phrases_json = response.json()
 
-    dialogflow_project_id = env.str('DIALOGFLOW_PROJECT_ID')
-    display_name = args.display_name
-    training_phrases_parts = phrases_json[display_name]['questions']
-    message_texts = [phrases_json[display_name]['answer']]
-    create_intent(dialogflow_project_id, display_name, training_phrases_parts, message_texts)
+    with open(args.intents_json, 'r', encoding="utf8") as json_file:
+        intents_json = json.load(json_file)
+
+    for intent_name, intent_raw in intents_json.items():
+        create_intent(dialogflow_project_id, intent_name, intent_raw)
 
 
 if __name__ == '__main__':
