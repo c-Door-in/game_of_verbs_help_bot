b4a29a2bd4ae25f73cea48b4d3578554452b875a
c_Door in <egor1@mail.ru>
2022-06-08 11:44:35 +0300

    Move log handlers to separate file

--- tg_bot.py
+++ tg_bot.py
@@ -7,20 +7,10 @@ from google.cloud import dialogflow
 from telegram import ForceReply
 from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
 
+from loghandlers import TelegramLogsHandler
+
 
 logger = logging.getLogger(__name__)
-
-
-class TelegramLogsHandler(logging.Handler):
-
-    def __init__(self, tg_bot, chat_id):
-        super().__init__()
-        self.chat_id = chat_id
-        self.tg_bot = tg_bot
-
-    def emit(self, record):
-        log_entry = self.format(record)
-        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def start(update, context):
--- vk_bot.py
+++ vk_bot.py
@@ -9,20 +9,10 @@ from environs import Env
 from google.cloud import dialogflow
 from vk_api.longpoll import VkLongPoll, VkEventType
 
+from loghandlers import TelegramLogsHandler
+
 
 logger = logging.getLogger(__name__)
-
-
-class TelegramLogsHandler(logging.Handler):
-
-    def __init__(self, tg_bot, chat_id):
-        super().__init__()
-        self.chat_id = chat_id
-        self.tg_bot = tg_bot
-
-    def emit(self, record):
-        log_entry = self.format(record)
-        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def detect_intent_texts(project_id, session_id, text, language_code):
